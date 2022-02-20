35e4d56bd1f8b635854174b955b47089fd4fc181
choi sangwon <89228714+csw180@users.noreply.github.com>
2022-02-20 18:33:26 +0900

    Add files via upload

--- trader1.py
+++ trader1.py
@@ -1,19 +1,22 @@
 import time
 import pyupbit
 import datetime as dt
-import BestXnBase
+import XnBase
+
 
 access = "Oug97pOTCd6xN12mREWTo9GTQcmkzhMtnoW2Wqyo"          # 본인 값으로 변경
 secret = "6IUBTLNU02rSGQux5cIMW11W05WnoW5rRKxxSE6Z"          # 본인 값으로 변경
 
 k = 0.5
 base = 21
-current_ticker = 'KRW-ETC'
-currency = 'ETC'
+current_ticker = 'KRW-EOS'
+currency = current_ticker[-3:]
+baseday = dt.datetime.now()
+
 
 def get_target_price(ticker, k,base):
     """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = BestXnBase.get_ohlcv_custom(ticker,20,base)
+    df = XnBase.get_ohlcv_custom(ticker,20,base)
     df['range'] = (df['high'] - df['low']) * k
     df['target'] = df['open'] + df['range'].shift(-1)
     target_price = df.iloc[0]['target'] 
@@ -24,15 +27,16 @@ def get_start_time(base) :
 
     start_time = basetime.replace(hour=base,minute=0,second=0)
     if start_time > basetime :
+        nextday = start_time
         end_time = start_time - dt.timedelta(seconds=10)
         start_time = start_time - dt.timedelta(days=1)
         
     else :
+        nextday = start_time + dt.timedelta(days=1)
         end_time = start_time + dt.timedelta(days=1) - dt.timedelta(seconds=10)
 
-    # print(f'get_start_time:start_time = {start_time}, end_time = {end_time}')
-    return start_time, end_time, basetime
-
+    # print(f'get_start_time:start_time = {start_time}, end_time = {end_time}, nextday={nextday}', flush=True)
+    return start_time, end_time, nextday
 
 def get_balance(ticker):
     """잔고 조회"""
@@ -45,41 +49,60 @@ def get_balance(ticker):
                 return 0
     return 0
 
-def get_current_price(ticker):
+def get_current_ask_price(ticker):  # 현재 매수가.
     """현재가 조회"""
     return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
 
+def get_current_bid_price(ticker):  #현재 매도가
+    """현재가 조회"""
+    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["bid_price"]
+
 # 로그인
+XnBase.current_ticker = current_ticker
+print(XnBase.current_ticker)
+
 upbit = pyupbit.Upbit(access, secret)
-print("Autotrader init.. best K, base simmulation.....")
-k , base = BestXnBase.bestValue()
-print(f'k = {k}, base = {base}')
-# k , base = 0.1, 22
+
+print("Autotrader init.. ", flush=True)
 # 자동매매 시작
-loop_cnt = 0
-while True:
-    try:
-        loop_cnt +=1
-        start_time, end_time, nw  = get_start_time(base)
-        if start_time < nw < end_time :
-            target_price = get_target_price(current_ticker, k,base)
-            current_price = get_current_price(current_ticker)
-            if loop_cnt >= 10 :   # 운영모드로 가면 충분히 크게 바꿀것..
-                print(f'Now : {nw:%Y-%m-%d %H:%M:%S}, start : {start_time:%Y-%m-%d %H:%M:%S}, end : {end_time::%Y-%m-%d %H:%M:%S}, target:{target_price:.2f}, current:{current_price}')
-                loop_cnt = 0
-            if target_price < current_price:
-                krw = get_balance("KRW")
-                print(f'get_balance(KRW): {krw}')
-                if krw > 5000:
-                    print(f'buy_market_order {current_ticker}, {krw*0.9995:.2f}')
-                #    upbit.buy_market_order(current_ticker, krw*0.9995)
-        else:
-            btc = get_balance(currency)
-            print(f'get_balance(BTC): {btc}')
-            if btc > ( 5000 / current_price) :
-                print(f'sell_market_order {current_ticker}, {btc*0.9995:.2f}')
-                upbit.sell_market_order(current_ticker, btc*0.9995)
-        time.sleep(1)
-    except Exception as e:
-        print(e)
-        time.sleep(1)
+while  True :
+    print("best K, base simmulation.....", flush=True)
+    k , base = XnBase.bestValue()
+    print(f'k = {k}, base = {base}', flush=True)
+    start_time, end_time, nextday  = get_start_time(base)
+    print(f'Day START!..{start_time:%Y-%m-%d %H:%M:%S} ~ {end_time::%Y-%m-%d %H:%M:%S},  nextday : {nextday:%Y-%m-%d %H:%M:%S}', flush=True)
+
+    target_price = get_target_price(current_ticker, k,base)
+    print(f'target_price = {target_price}', flush=True)
+    current_time = dt.datetime.now()
+    loop_cnt = 0
+    while current_time < nextday :
+        try:
+            loop_cnt +=1
+            if start_time < current_time < end_time :
+                current_price = get_current_ask_price(current_ticker)
+                if loop_cnt >= 10 :   # 운영모드로 가면 충분히 크게 바꿀것..
+                    print(f'Now : {current_time:%Y-%m-%d %H:%M:%S}, start : {start_time:%Y-%m-%d %H:%M:%S}, end : {end_time:%Y-%m-%d %H:%M:%S}, target:{target_price:,.2f}, current:{current_price:,}', flush=True)
+                    loop_cnt = 0
+                if target_price < current_price:
+                    krw = get_balance("KRW")
+                    print(f'get_balance(KRW): {krw}', flush=True)
+                    if krw > 5000:
+                        ret = upbit.buy_limit_order(current_ticker, current_price, (krw*0.9995)//current_price )
+                        print(f'buy_limit_order {current_ticker}, {krw*0.9995:.2f}', flush=True)
+                        print(f'buy_limit_order ret = {ret}' , flush=True)
+            else:
+                btc = get_balance(currency)
+                print(f'get_balance(BTC): {btc}', flush=True)
+                current_price = get_current_bid_price(current_ticker)
+                if btc > ( 5000 / current_price) :
+                    ret = upbit.sell_limit_order(current_ticker, current_price, (btc*0.9995)//current_price )
+                    print(f'sell_limit_order {current_ticker}, {btc*0.9995:.2f}', flush=True)
+                    print(f'sell_limit_order ret = {ret}' , flush=True)
+            time.sleep(1)
+        except Exception as e:
+            print(e, flush=True)
+            time.sleep(1)
+
+        current_time = dt.datetime.now()
+
