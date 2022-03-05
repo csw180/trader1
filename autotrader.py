ca0f18a0d24feebd79065aefdf6346a23b387f1d
choi sangwon <89228714+csw180@users.noreply.github.com>
2022-03-05 18:02:19 +0900

    Add files via upload

--- autotrader.py
+++ autotrader.py
@@ -88,10 +88,11 @@ while  True :
 
             elif  t.start_time < current_time < t.end_time :    
                 # 이미 잔고가 있는 종목은 목표가에 왔는지 확인하고 즉시 매도 처리 한다.
-                if  btc:=upbit_trade.get_balance(t.currency) > 0 :
+                btc=upbit_trade.get_balance(t.currency)
+                if  btc > 0 :
                     current_price = float(pyupbit.get_orderbook(ticker=t.name)["orderbook_units"][0]["bid_price"])
                     avg_buy_price = upbit_trade.get_avg_buy_price(t.currency)
-                    if loop_cnt >= print_loop-5 :   # 운영모드로 가면 충분히 크게 바꿀것..
+                    if print_loop-5 <= loop_cnt < print_loop-3 :   # 운영모드로 가면 충분히 크게 바꿀것..
                         print_(t.name,f'Enough price TEST btc=({t.currency}): {btc}, avg_buy_price = {avg_buy_price}, current_price= {current_price}')
                     if  current_price > avg_buy_price * 1.025 :
                         upbit_trade.sell_limit_order(t.name, current_price, btc )
@@ -112,7 +113,8 @@ while  True :
                     if krw > 5000:
                         upbit_trade.buy_limit_order(t.name, current_price, (krw*0.999)//current_price )
             else : 
-                if  btc:=upbit_trade.get_balance(t.currency) > 0 :
+                btc=upbit_trade.get_balance(t.currency) 
+                if  btc > 0 :
                     current_price = float(pyupbit.get_orderbook(ticker=t.name)["orderbook_units"][0]["bid_price"])
                     print_(t.name,f'force to sell unconditionally get_balance({t.currency}): {btc}, current_price= {current_price}')
                     upbit_trade.sell_limit_order(t.name, current_price, btc )
